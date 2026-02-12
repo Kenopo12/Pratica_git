3d8f873e6892cdf6f27c764b95afbc78cb43d94a
Daw1 <Daw1@MEDREGAL14.lasgalletas.lan>
2026-02-12 20:12:51 +0000

    primer cambio

---
+++ src/pratica_git/Pratica_git.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pratica_git;
+
+/**
+ *
+ * @author JuanJo Hurtado
+ */
+public class Pratica_git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo este es mi primer repositorio");
+    
+    }
+}
